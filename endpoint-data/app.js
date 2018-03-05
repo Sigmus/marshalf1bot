0ab0774e61c9ad1692160566dee60d2e00ad0c29
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-05 08:30:08 +0000

    Add endpoint-data boilerplate

---
+++ endpoint-data/app.js
@@ -0,0 +1,10 @@
+// https://a6fhgm3mmj.execute-api.eu-west-1.amazonaws.com/latest/hello
+
+const ApiBuilder = require("claudia-api-builder");
+const api = new ApiBuilder();
+
+module.exports = api;
+
+api.get("/hello", () => {
+  return "hello world";
+});

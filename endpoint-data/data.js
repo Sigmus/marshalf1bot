0ad90f9e2c814e00185a03e483203f1256814b14
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-05 11:52:04 +0000

    Save json if diff > 5

--- endpoint-data/data.js
+++ endpoint-data/data.js
@@ -13,8 +13,8 @@ const ergast = require("./ergast");
 
 const s3 = new AWS.S3();
 
-module.exports = () =>
-  Promise.all([
+module.exports = () => {
+  return Promise.all([
     ergast("2017/driverStandings").then(
       x => x.MRData.StandingsTable.StandingsLists[0].DriverStandings
     ),
@@ -51,28 +51,42 @@ module.exports = () =>
       }
     );
 
-    data = {
-      rounds,
-      standings: {
-        drivers,
-        constructors
-      }
-    };
+    data = { rounds, standings: { drivers, constructors } };
 
-    const params = {
-      Bucket: "marshalf1bot",
-      Key: "data.json",
-      Body: JSON.stringify(data, null, 4)
-    };
+    const params = { Bucket: "marshalf1bot", Key: "data.json" };
 
-    return new Promise((resolve, reject) => {
-      s3.putObject(params, err => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Successfully uploaded data to myBucket/myKey");
-          resolve(data);
-        }
+    const writeFile = Body =>
+      new Promise((resolve, reject) => {
+        params.Body = Body;
+        s3.putObject(params, err => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Successfully uploaded data to myBucket/myKey");
+            resolve(data);
+          }
+        });
       });
-    });
+
+    const readFile = () =>
+      new Promise((resolve, reject) => {
+        s3.getObject(params, (err, _data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const diff = moment().diff(moment(_data.LastModified), "minutes");
+            console.log("diff", diff);
+            if (diff > 2) {
+              writeFile(JSON.stringify(data, null, 4)).then(() =>
+                resolve(data)
+              );
+            } else {
+              resolve(data);
+            }
+          }
+        });
+      });
+
+    return readFile();
   });
+};

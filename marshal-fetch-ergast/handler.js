f3ae82036bed7755272352747ab7c2662013c866
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-07 08:36:48 +0000

    Rename serverless function

--- marshal-fetch-ergast/handler.js
+++ marshal-fetch-ergast/handler.js
@@ -4,7 +4,7 @@ const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
 module.exports.refresh = (event, context, callback) => {
-  fetchRemote().then(() =>
+  fetchRemote().then(data =>
     s3.putObject(
       {
         Bucket: "marshalf1bot",

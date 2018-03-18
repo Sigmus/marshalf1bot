b384fc344979bd0cafff615a271cc6453776db97
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-18 15:45:02 +0000

    Fetch remote data

--- marshal-bot/bot.js
+++ marshal-bot/bot.js
@@ -1,5 +1,24 @@
+const AWS = require("aws-sdk");
 const botBuilder = require("claudia-bot-builder");
 
-module.exports = botBuilder(function(request) {
-  return "Pong: " + request.text;
+const s3 = new AWS.S3();
+
+module.exports = botBuilder(request => {
+  return new Promise((resolve, reject) => {
+    s3.getObject(
+      {
+        Bucket: "marshalf1bot",
+        Key: "data.json"
+      },
+      (err, response) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          const data = response.Body.toString();
+          resolve("Pong: " + request.text);
+        }
+      }
+    );
+  });
 });

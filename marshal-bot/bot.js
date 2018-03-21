515800f987e4473e0ba463d1cc75ded7cc40e63e
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-21 08:39:35 +0000

    Convert main function to promise

--- marshal-bot/bot.js
+++ marshal-bot/bot.js
@@ -1,30 +1,23 @@
-const AWS = require("aws-sdk");
 const botBuilder = require("claudia-bot-builder");
+const AWS = require("aws-sdk");
+const router = require("./router");
 
 const s3 = new AWS.S3();
 
-const router = require("./router");
-
 module.exports = botBuilder(
   request => {
-    return new Promise((resolve, reject) => {
-      s3.getObject(
-        {
-          Bucket: "marshalf1bot",
-          Key: "data.json"
-        },
-        (err, response) => {
-          if (err) {
-            console.log(err);
-            reject(err);
-          } else {
-            const data = JSON.parse(response.Body.toString());
-            const cmd = request.text.toLowerCase();
-            resolve(router(cmd, data));
-          }
-        }
-      );
-    });
+    return s3
+      .getObject({ Bucket: "marshalf1bot", Key: "data.json" })
+      .promise()
+      .then(response => {
+        const data = JSON.parse(response.Body.toString());
+        const cmd = request.text.toLowerCase();
+        return router(cmd, data);
+      })
+      .catch(err => {
+        console.log(err);
+        return err;
+      });
   },
   { platforms: ["facebook"] }
 );

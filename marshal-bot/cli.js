6ccfab2ea690cf183f485a4103ca2bbc8bcae402
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-21 19:25:56 +0000

    Update comments in cli.js

--- marshal-bot/cli.js
+++ marshal-bot/cli.js
@@ -3,11 +3,11 @@ var argv = require("minimist")(process.argv.slice(2));
 const router = require("./router");
 const data = require("./data");
 
-// node test.js --cmd 'next'
-// node test.js --cmd 'drivers'
+// node cli.js --cmd 'next'
+// node cli.js --cmd 'drivers'
 
 if (!argv.cmd) {
-  console.log("Usage: php test.js --cmd something");
+  console.log('Usage: php cli.js --cmd "something"');
   process.exit(1);
 }
 

6cf7a691ea3174ed686cc37226c97055743f33da
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-22 07:45:23 +0000

    Put back refresh logic

--- marshal-fetch-ergast/handler.js
+++ marshal-fetch-ergast/handler.js
@@ -5,6 +5,15 @@ const s3 = new AWS.S3();
 
 module.exports.refresh = (event, context, callback) => {
   fetchRemote().then(data =>
-
+    s3.putObject(
+      {
+        Bucket: "marshalf1bot",
+        Key: "data.json",
+        Body: JSON.stringify(data),
+        ACL: "public-read"
+      },
+      err =>
+        err ? console.log(err) : callback(null, { message: "data-refreshed" })
+    )
   );
 };

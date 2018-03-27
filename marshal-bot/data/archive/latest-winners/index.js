c84011346587b4afda1d4bc0bf3ec859bc68cfe6
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-27 11:12:58 +0100

    Add latest-winnners script

---
+++ marshal-bot/data/archive/latest-winners/index.js
@@ -0,0 +1,27 @@
+const _ = require("lodash");
+
+const results = {
+  2013: require("../2013/results"),
+  2014: require("../2014/results"),
+  2015: require("../2015/results"),
+  2016: require("../2016/results"),
+  2017: require("../2017/results")
+};
+
+const winners = _.reduce(
+  results,
+  (acc, next, year) => {
+    next.forEach(race => {
+      acc[race.slug] = acc[race.slug] || [];
+      acc[race.slug].push({
+        year,
+        race: race.slug,
+        driver: race.results[0].Driver.familyName
+      });
+    });
+    return acc;
+  },
+  {}
+);
+
+console.log(JSON.stringify(winners, null, 4));

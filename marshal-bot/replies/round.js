b6c22928df075531c77f172f208573137a5ffd27
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-28 12:36:41 +0100

    Show last winners by default

--- marshal-bot/replies/round.js
+++ marshal-bot/replies/round.js
@@ -3,6 +3,7 @@ const size = require("lodash/size");
 const moment = require("moment");
 const fetchS3 = require("../data/fetch-s3");
 const currentSeason = require("../data/archive/2018/season");
+const latestWinners = require("../data/archive/latest-winners");
 
 module.exports = roundIndex => {
   return Promise.all([
@@ -12,23 +13,29 @@ module.exports = roundIndex => {
     const qualifying = response[0][roundIndex] ? response[0][roundIndex] : [];
     const results = response[1][roundIndex] ? response[1][roundIndex] : [];
 
-    const obj = new fbTemplate.Button(
-      `${currentSeason[roundIndex].title}\n${moment
-        .unix(currentSeason[roundIndex].ts)
-        .format("MMM Do, LT")} – Round ${currentSeason[roundIndex].round}`
-    );
+    let content = `${currentSeason[roundIndex].title} Grand Prix\n${moment
+      .unix(currentSeason[roundIndex].ts)
+      .format("MMM Do, LT")} – Round ${currentSeason[roundIndex].round}`;
+
+    if (!size(results)) {
+      content +=
+        "\n\nLast Winners\n" +
+        latestWinners[currentSeason[roundIndex].slug]
+          .reverse()
+          .map(item => `${item.season}. ${item.driver}`)
+          .join("\n");
+    }
+
+    const obj = new fbTemplate.Text(content);
 
     if (size(qualifying) > 0) {
-      obj.addButton("Qualifying", `qualifying ${roundIndex + 1}`);
+      obj.addQuickReply("Qualifying", `qualifying ${roundIndex + 1}`);
     }
 
     if (size(results) > 0) {
-      obj.addButton("Race Results", `results ${roundIndex + 1}`);
-    } else {
-      obj.addButton("Latest Winners", `winners ${roundIndex + 1}`);
+      obj.addQuickReply("Race Results", `results ${roundIndex + 1}`);
     }
-
-    obj.addButton("Remaining races", "remaining");
+    obj.addQuickReply("Remaining races", "remaining");
 
     return obj.get();
   });

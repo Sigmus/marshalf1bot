b5f3aa3e2b7249509bd89491e99685d28e0fe615
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-28 13:56:32 +0100

    Remove past folder / fetch 2018

--- marshal-fetch-ergast/handler.js
+++ marshal-fetch-ergast/handler.js
@@ -1,18 +1,19 @@
-const fetchRemote = require("./fetch-remote-2018");
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3();
 
-module.exports.refresh = (event, context, callback) =>
-  fetchRemote().then(data =>
-    s3
-      .putObject({
-        Bucket: "marshalf1bot",
-        Key: "data.json",
-        Body: JSON.stringify(data),
-        ACL: "public-read"
-      })
-      .promise()
-      .then(() => callback(null, { message: "data-refreshed" }))
-      .catch(err => console.log(err))
-  );
+module.exports.refresh = (event, context, callback) => {
+  callback(null, { message: "todo!!!" });
+  // return fetchRemote().then(data =>
+  //   s3
+  //     .putObject({
+  //       Bucket: "marshalf1bot",
+  //       Key: "data.json",
+  //       Body: JSON.stringify(data),
+  //       ACL: "public-read"
+  //     })
+  //     .promise()
+  //     .then(() => callback(null, { message: "data-refreshed" }))
+  //     .catch(err => console.log(err))
+  // );
+};

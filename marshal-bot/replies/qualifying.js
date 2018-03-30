58fd03952b5f70e0d24c93aa5ffa0b04519d127a
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-03-30 12:29:37 +0100

    Qualifying show results if already available and vice-versa

--- marshal-bot/replies/qualifying.js
+++ marshal-bot/replies/qualifying.js
@@ -1,13 +1,29 @@
+const { fbTemplate } = require("claudia-bot-builder");
+const size = require("lodash/size");
 const fetchS3 = require("../data/fetch-s3");
 
 module.exports = roundIndex => {
-  return fetchS3("qualifying.json").then(response => {
-    const content = response[roundIndex].qualifying
+  return Promise.all([
+    fetchS3("qualifying.json"),
+    fetchS3("results.json")
+  ]).then(response => {
+    const qualifying = response[0][roundIndex] ? response[0][roundIndex] : [];
+    const results = response[1][roundIndex] ? response[1][roundIndex] : [];
+
+    const content = qualifying.qualifying
       .map(
         item => `${item.pos}. ${item.driver} – ${item.q3 || item.q2 || item.q1}`
       )
       .join("\n");
 
-    return `${response[roundIndex].title} Grand Prix Qualifying:\n\n${content}`;
+    const obj = new fbTemplate.Text(
+      `${qualifying.title} Grand Prix Qualifying:\n\n${content}`
+    );
+
+    if (size(results)) {
+      obj.addQuickReply("Race Results", `results ${roundIndex + 1}`);
+    }
+
+    return obj.get();
   });
 };
--- marshal-bot/replies/results.js
+++ marshal-bot/replies/results.js
@@ -1,8 +1,16 @@
+const { fbTemplate } = require("claudia-bot-builder");
+const size = require("lodash/size");
 const fetchS3 = require("../data/fetch-s3");
 
 module.exports = roundIndex => {
-  return fetchS3("results.json").then(response => {
-    const content = response[roundIndex].results
+  return Promise.all([
+    fetchS3("qualifying.json"),
+    fetchS3("results.json")
+  ]).then(response => {
+    const qualifying = response[0][roundIndex] ? response[0][roundIndex] : [];
+    const results = response[1][roundIndex] ? response[1][roundIndex] : [];
+
+    const content = results.results
       .map(
         item =>
           `${item.pos}. ${item.driver} ${
@@ -11,6 +19,12 @@ module.exports = roundIndex => {
       )
       .join("\n");
 
-    return `${response[roundIndex].title} Results:\n\n${content}`;
+    const obj = new fbTemplate.Text(`${results.title} Results:\n\n${content}`);
+
+    if (size(qualifying)) {
+      obj.addQuickReply("Qualifying", `qualifying ${roundIndex + 1}`);
+    }
+
+    return obj.get();
   });
 };

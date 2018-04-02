bbcdcf519e434556307a8a1b6915e490bd4bfe15
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-04-02 13:51:06 +0100

    Remove fetchS3

--- marshal-bot/data/fetch-s3.js
+++
@@ -1,9 +0,0 @@
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
-const season = require("./season");
-
-module.exports = Key =>
-  s3
-    .getObject({ Bucket: "marshalf1bot", Key: `${season.year}/${Key}` })
-    .promise()
-    .then(response => JSON.parse(response.Body.toString()));

50628bb4f903be18fa991d83eb8fcd840dd3e99c
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-04-02 14:46:53 +0100

    Org cli.js

--- marshal-bot/cli.js
+++ marshal-bot/cli.js
@@ -2,17 +2,10 @@ const argv = require("minimist")(process.argv.slice(2));
 const router = require("./router");
 const processConvesation = require("./process-conversation");
 
-// node cli.js --cmd 'next'
-
 if (!argv.cmd) {
   console.log('Usage: php cli.js --cmd "something"');
   process.exit(1);
 }
-
-const printResponse = response =>
-  console.log(
-    typeof response === "string" ? response : JSON.stringify(response, null, 4)
-  );
 
 const message = {
   text: argv.cmd,
@@ -21,5 +14,11 @@ const message = {
 };
 
 processConvesation(message).then(previousMessage => {
-  router(message, previousMessage).then(printResponse);
+  router(message, previousMessage).then(response =>
+    console.log(
+      typeof response === "string"
+        ? response
+        : JSON.stringify(response, null, 4)
+    )
+  );
 });

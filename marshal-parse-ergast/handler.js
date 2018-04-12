86f44760aa870e22ad5f586a4fa11b289b259ff5
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-04-12 10:12:04 +0100

    Add message proto

--- marshal-parse-ergast/handler.js
+++ marshal-parse-ergast/handler.js
@@ -1,9 +1,13 @@
 const parse = require("./parse");
+const message = require("./message");
 
 module.exports.parse = (event, context, callback) => {
   const keys = event.Records.map(i => i.dynamodb.Keys.endpoint.S);
-  Promise.all(keys.map(parse)).then(response => {
-    console.log(JSON.stringify({ keys, response }, null, 4));
-    callback(null, { message: "marshal-parse-ergast", keys });
+  Promise.all(keys.map(key => parse(key, 2))).then(response => {
+    Promise.all(
+      response
+        .filter(i => i.areDifferent === true)
+        .map(i => message(`New results for ${i.key}`))
+    ).then(() => callback(null, { message: "marshal-parse-ergast", keys }));
   });
 };

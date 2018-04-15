4814c88cb677daeaedc76d402fe3aa1629db61e2
Flávio Atas Medeiros <Flavio.Medeiros@mullenloweprofero.com>
2018-04-15 10:51:39 +0100

    Add pagination CTA to drivers

--- marshal-bot/replies/drivers.js
+++ marshal-bot/replies/drivers.js
@@ -5,15 +5,27 @@ const settings = require("../settings");
 
 module.exports = offset => {
   const pagesize = parseInt(settings.getKey("pagesize"));
+
   return db.fetchItem(`${season.year}/driverStandings`).then(data => {
+    const total = data.length;
+    const startpos = offset * pagesize;
+    const endpos = offset * pagesize + pagesize;
+
     const content = data
-      .slice(offset * pagesize, offset * pagesize + pagesize)
+      .slice(startpos, endpos)
       .map(row => `${row.pos}. ${row.driver} – ${row.points}`)
       .join("\n");
 
     const newMessage = new fbTemplate.Text(
       `${season.year} Driver's Championship:\n\n${content}`
     );
+
+    if (endpos < total) {
+      newMessage.addQuickReply(
+        `+ ${pagesize}`,
+        `drivers ${parseInt(offset, 10) + 1}`
+      );
+    }
 
     newMessage.addQuickReply("Constructors", "constructors");
     newMessage.addQuickReply("Next race", "next");
